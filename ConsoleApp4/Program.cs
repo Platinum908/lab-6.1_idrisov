408f6b1e6aa5fa5b76efa34fd4e289f0a5bd75d5
Platinum908 <estnuntius@gmail.com>
2023-12-23 23:37:13 +0200

    Добавьте файлы проекта.

---
+++ ConsoleApp4/Program.cs
@@ -0,0 +1,24 @@
+﻿//Лаб 6.1 Высокий 24
+
+Console.Write("Введите равное кол-во делителей: ");
+int k = int.Parse(Console.ReadLine());
+Console.WriteLine("Трехзначные числа с {0} делителями:", k);
+for (int num = 100; num <= 999; num++)
+{
+    if (divisors(num) == k)
+    {
+        Console.WriteLine(num);
+    }
+}
+int divisors(int number)
+{
+    int count = 0;
+    for (int i = 1; i <= number; i++)
+    {
+        if (number % i == 0)
+        {
+            count++;
+        }
+    }
+    return count;
+}
